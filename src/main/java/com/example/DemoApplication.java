d62a3b59d8b12b6b3f6ceaa085b07ea0b6237e7f
zcfrank1st <zcfrank1st@sina.com>
2015-12-10 10:12:02 +0800

    change logback config

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(DemoApplication.class);
         springApplication.run(args);
+        log.info("==== application start ====");
     }
 }
